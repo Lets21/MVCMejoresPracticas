50a95dcd244f7118bdbab97b2c572a251d557745
Lets21 <leito.t210403@outlook.es>
2025-01-26 16:59:38 -0500

    Tercer  commit

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+python manage.py flush    
+python manage.py crear_datos_iniciales
